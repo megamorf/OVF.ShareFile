e60215dff6db444add0ea41a264916318d1490fa
Sebastian N <basti.megamorf+github-com@googlemail.com>
2017-01-16 22:53:02 +0100

    Reorganize psdeploy file

--- module.psdeploy.ps1
+++ module.psdeploy.ps1
@@ -1,11 +1,45 @@
-if($ENV:BHProjectName -and $ENV:BHProjectName.Count -eq 1) {
-    Deploy OVF-Module {
+# Generic module deployment.
+#
+# ASSUMPTIONS:
+#
+# * folder structure either like:
+#
+#   - RepoFolder
+#     - This PSDeploy file
+#     - ModuleName
+#       - ModuleName.psd1
+#
+#   OR the less preferable:
+#   - RepoFolder
+#     - RepoFolder.psd1
+#
+# * Nuget key in $ENV:NugetApiKey
+#
+# * Set-BuildEnvironment from BuildHelpers module has populated ENV:BHPSModulePath and related variables
+
+# Publish to gallery with a few restrictions
+if(
+    $env:BHPSModulePath -and
+    $env:BHBuildSystem -ne 'Unknown' -and
+    $env:BHBranchName -eq "master" -and
+    $env:BHCommitMessage -match '!deploy'
+)
+{
+    Deploy Module {
         By PSGalleryModule {
-            FromSource $ENV:BHProjectName
+            FromSource $ENV:BHPSModulePath
             To PSGallery
             WithOptions @{
                 ApiKey = $env:PSGalleryApiKey
             }
         }
     }
+}
+else
+{
+    "Skipping deployment: To deploy, ensure that...`n" +
+    "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
+    "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
+    "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)" |
+        Write-Host
 }
