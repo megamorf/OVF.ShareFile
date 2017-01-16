99d97b0990040f87d59cf27397b4becb85b3a0f0
Sebastian N <basti.megamorf+github-com@googlemail.com>
2017-01-16 21:46:23 +0100

    Publish initial release of OVF.ShareFile !deploy

--- module.psdeploy.ps1
+++ module.psdeploy.ps1
@@ -1,9 +1,8 @@
 if($ENV:BHProjectName -and $ENV:BHProjectName.Count -eq 1) {
     Deploy OVF-Module {
-        By PSGalleryModule Artifactory {
+        By PSGalleryModule {
             FromSource $ENV:BHProjectName
             To PSGallery
-            Tagged PSGallery
             WithOptions @{
                 ApiKey = $env:PSGalleryApiKey
             }
