b9991b01706578a8028ab1b893ae661825e7e572
Sebastian N <basti.megamorf+github-com@googlemail.com>
2017-01-16 22:20:59 +0100

    Publish initial release of OVF.ShareFile !deploy

--- build.ps1
+++ build.ps1
@@ -1,53 +1,53 @@
-[cmdletbinding()]
-param(
-    [string[]]$Task = 'default'
-)
-
-function Resolve-Module {
+﻿function Resolve-Module
+{
     [Cmdletbinding()]
-    param (
-        [Parameter(Mandatory, ValueFromPipeline)]
+    param
+    (
+        [Parameter(Mandatory)]
         [string[]]$Name
     )
-
-    Process {
-        foreach ($ModuleName in $Name) {
+    Process
+    {
+        foreach ($ModuleName in $Name)
+        {
             $Module = Get-Module -Name $ModuleName -ListAvailable
-            Write-Verbose -Message "Resolving Module [$($ModuleName)]"
+            Write-Verbose -Message "Resolving Module $($ModuleName)"
 
-            if ($Module) {
+            if ($Module)
+            {
                 $Version = $Module | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
-                $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery -Verbose:$false |
-                    Measure-Object -Property Version -Maximum |
-                    Select-Object -ExpandProperty Maximum
+                $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
 
-                if ($Version -lt $GalleryVersion) {
+                if ($Version -lt $GalleryVersion)
+                {
+                    if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') { Set-PSRepository -Name PSGallery -InstallationPolicy Trusted }
+
                     Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
-
-                    Install-Module -Name $ModuleName -Verbose:$false -Force
-                    Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $GalleryVersion
+                    Install-Module -Name $ModuleName -Force
+                    Import-Module -Name $ModuleName -Force -RequiredVersion $GalleryVersion
                 }
-                else {
-                    Write-Verbose -Message "Module Installed, Importing [$($ModuleName)]"
-                    Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $Version
+                else
+                {
+                    Write-Verbose -Message "Module Installed, Importing $($ModuleName)"
+                    Import-Module -Name $ModuleName -Force -RequiredVersion $Version
                 }
             }
-            else {
-                Write-Verbose -Message "[$($ModuleName)] Missing, installing Module"
-                Install-Module -Name $ModuleName -Verbose:$false -Force
-                Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $Version
+            else
+            {
+                Write-Verbose -Message "$($ModuleName) Missing, installing Module"
+                Install-Module -Name $ModuleName -Force
+                Import-Module -Name $ModuleName -Force -RequiredVersion $Version
             }
         }
     }
 }
 
-Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+# Grab nuget bits, install modules, set build variables, start build.
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-'BuildHelpers', 'psake' | Resolve-Module
+Resolve-Module Psake, PSDeploy, Pester, BuildHelpers
 
 Set-BuildEnvironment
 
-Invoke-psake -buildFile "$PSScriptRoot\psake.ps1" -taskList $Task -nologo -Verbose:$VerbosePreference
+Invoke-psake .\psake.ps1
 exit ( [int]( -not $psake.build_success ) )
-
